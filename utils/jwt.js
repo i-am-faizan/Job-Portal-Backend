1c8cb5111e9356ab1003a1e8ff380c2c56603814
i-am-faizan <faizan0003khan@gmail.com>
2025-12-29 23:39:57 +0530

    Update jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,14 +1,16 @@
-export const sendToken = (user, statusCode, res, message) =>{
+export const sendToken = (user, statusCode, res, message) => {
     const token = user.getJWTToken();
     const options = {
-        erpires:new Date(
-            Date.now() + process.env.COOKIE_EXPIRE *24 *60 *60 *1000
+        erpires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: process.env.NODE_ENV === "production" ? true : false,
     };
-    
-    res.status(statusCode).cookie("token",token,options).json({
-        success:true,
+
+    res.status(statusCode).cookie("token", token, options).json({
+        success: true,
         user,
         message,
         token,
